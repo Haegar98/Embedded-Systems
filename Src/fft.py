6e7c500cba154ee7e319aa46d7685e9045ca534f
XxmatrixenxX <Falko116@freenet.de>
2021-11-29 16:03:08 +0100

    updated fft

--- Src/fft.py
+++ Src/fft.py
@@ -1,19 +1,10 @@
-import pyaudio
-import wave
 import matplotlib.pyplot as plt
-import scipy.io.wavfile as wavfile
-import scipy
-import scipy.fftpack as fftpk
-import numpy as np
-
-s_rate, signal = wavfile.read('test.wav')
-FFT = abs(scipy.fft(signal))
-freqs = fftpk.fftfreq(len(FFT),(1.0/s_rate))
-plt.plot(freqs[range(len(FFT)//2)], FFT[range(len(FFT)//2)])
-plt.xlabel('Frequency(Hz)')
-
-plt.ylabel('Amplitude')
-
-plt.show
-
-rate, data = wavfile.read('test.wav')
+from scipy.fftpack import fft
+from scipy.io import wavfile # get the api
+fs, data = wavfile.read('test.wav') # load the data
+a = data.T[0] # this is a two channel soundtrack, I get the first track
+b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
+c = fft(b) # calculate fourier transform (complex numbers list)
+d = len(c)/2  # you only need half of the fft list (real signal symmetry)
+plt.plot(abs(c[:(d-1)]),'r')
+plt.show()
